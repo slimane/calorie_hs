14d2c0908c5eefd5fd5bb6e657d4b32c7d5531a3
kay <kays@live.it>
2014-04-09 15:44:19 +0900

    add nAdd function

--- src/Main.hs
+++ src/Main.hs
@@ -3,8 +3,15 @@ module Main where
 import Meal
 
 
+mm :: Num a => Maybe a -> Maybe a -> Maybe a
+Nothing `mm` _ = Nothing
+_ `mm` Nothing = Nothing
+Just a `mm` Just b = Just $ a + b
 
 
 main = do
-  putStrLn . show $ defaultNutrient {calorie = Just 334, protein = Just 7.5, fat = Just 5.4, carbon = Just 63.4}
-  putStrLn . show $ defaultNutrient {calorie = Just 308, protein = Just 44.1, fat = Just 2.4, carbon = Just 26.9}
+  let breakFirst = defaultNutrient {calorie = Just 334, protein = Just 7.5, fat = Just 5.4, carbon = Just 63.4}
+  let lunch = defaultNutrient {calorie = Just 308, protein = Just 44.1, fat = Just 2.4, carbon = Just 26.9}
+  putStrLn . show $ breakFirst
+  putStrLn . show $ lunch
+  putStrLn . show $ breakFirst `nAdd` lunch
--- src/Meal.hs
+++ src/Meal.hs
@@ -1,4 +1,7 @@
-module Meal (Nutrient, calorie, protein, fat, carbon, NutrientQuantity, defaultNutrient, Meal) where
+module Meal (Nutrient, calorie, protein, fat, carbon, NutrientQuantity, defaultNutrient, Meal, nAdd) where
+
+import Data.Monoid
+import Control.Applicative
 
 type NutrientQuantity = Maybe Float
 data Nutrient = Nutrient{calorie :: NutrientQuantity
@@ -6,8 +9,17 @@ data Nutrient = Nutrient{calorie :: NutrientQuantity
                         , fat :: NutrientQuantity
                         , carbon :: NutrientQuantity}
                       deriving(Show, Eq, Read, Ord)
+
+nAdd :: Nutrient -> Nutrient -> Nutrient
+a `nAdd` b = Nutrient{calorie  = cal, protein = p, fat = f, carbon = c}
+    where
+        a ++ b = (+) <$> a <*> b
+        cal = calorie a ++ calorie b
+        p = protein a ++ protein b
+        f = fat a ++ fat b
+        c = carbon a ++ carbon b
+
 data Meal = Meal{name :: String, nutrient :: Nutrient, date :: String} deriving(Show, Eq, Read, Ord)
 
 defaultNutrient :: Nutrient
 defaultNutrient = Nutrient{calorie = Nothing, protein = Nothing, fat = Nothing, carbon = Nothing}
-
