d685b8e4ec56b825f6729bf6d98f0e9e8fa9542b
kay <kay_s@live.ie>
2014-04-10 23:23:31 +0900

    [from now] 2014/04/10 23:23:30
    [36mdiff --git a/src/Main.hs b/src/Main.hs[m
    [36mindex 039010a..2bc701c 100644[m
    [36m--- a/src/Main.hs[m
    [36m+++ b/src/Main.hs[m
    [34m@@ -1,7 +1,7 @@[m
    [32m module Main where[m
    [32m [m
    [32m import Meal[m
    [1m-import Workout[m
    [4m+[m[4m--import Workout[m
    [32m import Util.Util[m
    [32m --import Data.Time[m
    [32m --import Data.Time.Lens[m
    [34m@@ -14,6 +14,7 @@[m[32m [m[33mmain = do[m
    [32m     let lunch1 = defaultNutrient {calorie = Just 319, protein = Just 4.9, fat = Just 7.9, carbon = Just 57.4}[m
    [32m     let lunch2 = defaultNutrient {calorie = Just (107 * 2), protein = Just (14.4 * 2), fat = Just (4.8 * 2), carbon = Just (1.48 * 2)}[m
    [32m     let dinner = defaultNutrient {calorie = Just 363, protein = Just 16.3, fat = Just 5.8, carbon = Just 59.1}[m
    [1m-    let total = show $ foldl1 nAdd [breakFirst, lunch1, lunch2, dinner][m
    [1m-    writeFile "c:/HOME/learning_haskell/calorie/daily_data/20140410.txt" total[m
    [4m+[m[4m    let dinner2 = defaultNutrient {calorie = Just 262, protein = Just 4.3, fat = Just 16.2, carbon = Just 24.7}[m
    [4m+[m[4m    let total = show $ foldl1 nAdd [breakFirst, lunch1, lunch2, dinner, dinner2][m
    [4m+[m[4m    writeFile "/Users/theatrical/learning_haskell/calorie_hs/daily_data/20140409.txt" total[m
    [32m     putStrLn . show $ (applyBothSide (+) $ (*24) . length ) "1234567" "89"[m

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,7 @@
 module Main where
 
 import Meal
-import Workout
+--import Workout
 import Util.Util
 --import Data.Time
 --import Data.Time.Lens
@@ -14,6 +14,7 @@ main = do
     let lunch1 = defaultNutrient {calorie = Just 319, protein = Just 4.9, fat = Just 7.9, carbon = Just 57.4}
     let lunch2 = defaultNutrient {calorie = Just (107 * 2), protein = Just (14.4 * 2), fat = Just (4.8 * 2), carbon = Just (1.48 * 2)}
     let dinner = defaultNutrient {calorie = Just 363, protein = Just 16.3, fat = Just 5.8, carbon = Just 59.1}
-    let total = show $ foldl1 nAdd [breakFirst, lunch1, lunch2, dinner]
-    writeFile "c:/HOME/learning_haskell/calorie/daily_data/20140410.txt" total
+    let dinner2 = defaultNutrient {calorie = Just 262, protein = Just 4.3, fat = Just 16.2, carbon = Just 24.7}
+    let total = show $ foldl1 nAdd [breakFirst, lunch1, lunch2, dinner, dinner2]
+    writeFile "/Users/theatrical/learning_haskell/calorie_hs/daily_data/20140409.txt" total
     putStrLn . show $ (applyBothSide (+) $ (*24) . length ) "1234567" "89"
