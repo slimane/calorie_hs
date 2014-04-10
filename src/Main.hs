de92e569518065ba3453a8f65710ccc6d13f79fc
kay <kay_s@live.ie>
2014-04-11 01:31:36 +0900

    [from now] 2014/04/11 01:31:36
    [36mdiff --git a/daily_data/20140410.txt b/daily_data/20140410.txt[m
    [36mindex 0a5bf8f..0159eb3 100644[m
    [36m--- a/daily_data/20140410.txt[m
    [36m+++ b/daily_data/20140410.txt[m
    [34m@@ -1 +1 @@[m
    [1m-Nutrient {calorie = Just 1773.0, protein = Just 63.1, fat = Just 67.4, carbon = Just 236.56}[m
    [32m\ No newline at end of file[m
    [4m+[m[4mNutrient {calorie = Just 1929.0, protein = Just 64.5, fat = Just 77.0, carbon = Just 256.76}[m
    [32m\ No newline at end of file[m
    [36mdiff --git a/src/Main.hs b/src/Main.hs[m
    [36mindex f28acf5..3c70d47 100644[m
    [36m--- a/src/Main.hs[m
    [36m+++ b/src/Main.hs[m
    [34m@@ -16,7 +16,7 @@[m[32m [m[33mmain = do[m
    [32m     let dinner = defaultNutrient {calorie = Just 363, protein = Just 16.3, fat = Just 5.8, carbon = Just 59.1}[m
    [32m     let dinner2 = defaultNutrient {calorie = Just 262, protein = Just 4.3, fat = Just 16.2, carbon = Just 24.7}[m
    [32m     let ice = defaultNutrient {calorie = Just 78, protein = Just 0.7, fat = Just 4.8, carbon = Just 10.1}[m
    [1m-    let dinner3 = ice `nMal` 5[m
    [4m+[m[4m    let dinner3 = ice `nMal` 7[m
    [32m     let total = show $ foldl1 nAdd [breakFirst, lunch1, lunch2, dinner, dinner2, dinner3][m
    [32m     writeFile "/Users/theatrical/learning_haskell/calorie_hs/daily_data/20140410.txt" total[m
    [32m     putStrLn . show $ (applyBothSide (+) $ (*24) . length ) "1234567" "89"[m

--- src/Main.hs
+++ src/Main.hs
@@ -16,7 +16,7 @@ main = do
     let dinner = defaultNutrient {calorie = Just 363, protein = Just 16.3, fat = Just 5.8, carbon = Just 59.1}
     let dinner2 = defaultNutrient {calorie = Just 262, protein = Just 4.3, fat = Just 16.2, carbon = Just 24.7}
     let ice = defaultNutrient {calorie = Just 78, protein = Just 0.7, fat = Just 4.8, carbon = Just 10.1}
-    let dinner3 = ice `nMal` 5
+    let dinner3 = ice `nMal` 7
     let total = show $ foldl1 nAdd [breakFirst, lunch1, lunch2, dinner, dinner2, dinner3]
     writeFile "/Users/theatrical/learning_haskell/calorie_hs/daily_data/20140410.txt" total
     putStrLn . show $ (applyBothSide (+) $ (*24) . length ) "1234567" "89"
