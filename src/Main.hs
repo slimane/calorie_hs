82e79338bed4aa50596e757617d7bcd76ba3d537
kay <kays@live.it>
2014-04-11 21:37:18 +0900

    20140410 21:31

--- src/Main.hs
+++ src/Main.hs
@@ -10,13 +10,13 @@ import Util.Util
 
 main :: IO ()
 main = do
-    let breakFirst = defaultNutrient {calorie = Just 225, protein = Just 5.3, fat = Just 3.9, carbon = Just 41.9}
-    let lunch1 = defaultNutrient {calorie = Just 319, protein = Just 4.9, fat = Just 7.9, carbon = Just 57.4}
-    let lunch2 = defaultNutrient {calorie = Just (107 * 2), protein = Just (14.4 * 2), fat = Just (4.8 * 2), carbon = Just (1.48 * 2)}
-    let dinner = defaultNutrient {calorie = Just 363, protein = Just 16.3, fat = Just 5.8, carbon = Just 59.1}
-    let dinner2 = defaultNutrient {calorie = Just 262, protein = Just 4.3, fat = Just 16.2, carbon = Just 24.7}
-    let ice = defaultNutrient {calorie = Just 78, protein = Just 0.7, fat = Just 4.8, carbon = Just 10.1}
-    let dinner3 = ice `nMal` 7
-    let total = show $ foldl1 nAdd [breakFirst, lunch1, lunch2, dinner, dinner2, dinner3]
-    writeFile "/Users/theatrical/learning_haskell/calorie_hs/daily_data/20140410.txt" total
+    let windows = "C:/HOME/learning_haskell/calorie/daily_data"
+    let mac = "$HOME/learning_haskell/calorie_hs/daily_data"
+    let breakFirst = defaultNutrient {calorie = Just 337, protein = Just 7.5, fat = Just 5.4, carbon = Just 63.4}
+    let lunch = defaultNutrient {calorie = Just 190, protein = Just 3.8, fat = Just 4.0, carbon = Just 36.6}
+    let butadon = defaultNutrient {calorie = Just 373, protein = Just 11.2, fat = Just 6.8, carbon = Just 68.5}
+    let beaf = defaultNutrient {calorie = Just 109, protein = Just 20.3, fat = Just 1.7, carbon = Just 3.0}
+    let total = show $ foldl1 nAdd [breakFirst, lunch, butadon, beaf]
+    writeFile (windows ++ "/" ++ "20140411.txt") total
+    putStrLn total
     putStrLn . show $ (applyBothSide (+) $ (*24) . length ) "1234567" "89"
