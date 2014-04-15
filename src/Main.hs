c7cb9fb5b93dde621dae2ecb3a6734a4cd192ddf
kay <kays@live.it>
2014-04-15 16:38:04 +0900

    20140415 16:31

--- src/Main.hs
+++ src/Main.hs
@@ -3,7 +3,25 @@ module Main where
 import qualified Meal as M
 import qualified Util.Util as U
 import qualified Util.Translate as T
-import qualified System.Directory as D
+import Data.Time
+import Control.Applicative
+
+
+filename :: IO String
+filename = getCurrentTime >>= return
+                                . (\(y, m, d ) -> show y ++ zeroFill2 m ++ zeroFill2 d)
+                                . toGregorian
+                                . utctDay
+        where
+            zeroFill2 = zeroFill 2
+
+zeroFill :: (Integral a, Show a) => Int -> a -> String
+zeroFill len n = if l >= len
+                    then n'
+                    else  (take (len - l) . repeat $ '0') ++ n'
+        where
+            n' = show n
+            l = length n'
 
 
 
@@ -11,6 +29,8 @@ main :: IO ()
 main = do
     let windows = "C:/HOME/learning_haskell/calorie/daily_data"
     let mac = "/Users/theatrical/learning_haskell/calorie_hs/daily_data"
-    let file = windows ++ "/" ++ "20140414.txt"
+    file <- pure (\x -> windows ++ "/" ++ x ++ ".txt") <*> filename
     meal <- M.insertMeal
     appendFile file . (++ "\n ") . show $ meal
+
+
